28fd1ac641e1f7cc3243f7857383da628c7fedfe
tomikahn <tomikahn2000@gmail.com>
2023-08-13 22:40:28 -0300

    cv update

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <>
+      <div className="landing">
+        My name is Tomas Kahn <br /> Welcome!
+      </div>
+      <div className="about-me">
+        <h1 className="title">About Me</h1>
+      </div>
+      <div className="abilities">
+        <h1 className="title">Abilities</h1>
+      </div>
+    </>
+  );
+};
+
+export default Landing;
