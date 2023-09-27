c0f5e04e08229f7fefef6589f0b57b00479da7a2
tomikahn <tomikahn2000@gmail.com>
2023-09-26 22:20:07 -0300

    update

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -66,18 +66,18 @@ const Landing = () => {
     };
   }, []);
 
-  // useEffect(() => {
-  //   // Check if the current URL contains the hash for the 'about' section
-  //   if (window.location.hash === "#about") {
-  //     if (isAboutVisible) {
-  //       // Scroll to the 'about' section smoothly
-  //       window.scrollTo({
-  //         top: document.getElementById("about").offsetTop,
-  //         behavior: "smooth",
-  //       });
-  //     }
-  //   }
-  // }, [isAboutVisible]);
+  useEffect(() => {
+    // Check if the current URL contains the hash for the 'about' section
+    if (window.location.hash === "#about") {
+      if (isAboutVisible) {
+        // Scroll to the 'about' section smoothly
+        window.scrollTo({
+          top: document.getElementById("about").offsetTop,
+          behavior: "smooth",
+        });
+      }
+    }
+  }, [isAboutVisible]);
 
   const skillImages = [
     "./assets/html.png",
@@ -156,20 +156,22 @@ const Landing = () => {
             justifyContent: "center",
           }}
         >
-          <motion.button
-            className="btn-pink"
-            whileHover={{ scale: 1.1 }} // Scale up on hover
-            whileTap={{ scale: 0.9 }} // Scale down on tap
-            onClick={() => {
-              setIsChargingVisible(true);
-              const introTimeout = setTimeout(() => {
-                setIsAboutVisible(true);
-              }, 1300);
-              navigate("/#about");
-            }}
-          >
-            Click Here to Build Page
-          </motion.button>
+          {isIntroVisible && (
+            <motion.button
+              className="btn-pink"
+              whileHover={{ scale: 1.1 }} // Scale up on hover
+              whileTap={{ scale: 0.9 }} // Scale down on tap
+              onClick={() => {
+                setIsChargingVisible(true);
+                const introTimeout = setTimeout(() => {
+                  setIsAboutVisible(true);
+                }, 1300);
+                navigate("/#about");
+              }}
+            >
+              Click Here to Build Page
+            </motion.button>
+          )}
         </div>
         <div className="terminal-text" style={{ textAlign: "center" }}>
           <TypewriterText
@@ -508,26 +510,86 @@ const Landing = () => {
             color: "white",
           }}
         >
-          <h2 style={{ paddingTop: "2rem", marginBottom:"2rem", fontSize:"2rem" }}>Contact</h2>
-          <div className="links" style={{paddingBottom:"2rem", display:"flex", width:"99.1vw", margin:"0 auto", alignItems:"center", justifyContent:"center"}}>
+          <h2
+            style={{
+              paddingTop: "2rem",
+              marginBottom: "2rem",
+              fontSize: "2rem",
+            }}
+          >
+            Contact
+          </h2>
+          <div
+            className="links"
+            style={{
+              paddingBottom: "2rem",
+              display: "flex",
+              width: "99.1vw",
+              margin: "0 auto",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
             <a
-              style={{ color: "white", marginLeft:"1rem", marginRight:"1rem" }}
+              style={{
+                color: "white",
+                marginLeft: "1rem",
+                marginRight: "1rem",
+              }}
               href="https://www.linkedin.com/in/tomas-kahn/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" height="2.5em" viewBox="0 0 448 512"><path fill="#fff" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="2.5em"
+                viewBox="0 0 448 512"
+              >
+                <path
+                  fill="#fff"
+                  d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"
+                />
+              </svg>
             </a>
             <a
-              style={{ color: "white", marginLeft:"1rem", marginRight:"1rem" }}
+              style={{
+                color: "white",
+                marginLeft: "1rem",
+                marginRight: "1rem",
+              }}
               href="https://wa.me/3516791729"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" height="2.5em" viewBox="0 0 448 512"><path fill="#fff" d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"/></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="2.5em"
+                viewBox="0 0 448 512"
+              >
+                <path
+                  fill="#fff"
+                  d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"
+                />
+              </svg>
             </a>
-            <a style={{ color: "white", marginLeft:"1rem", marginRight:"1rem" }} href="mailto:tomikahn2000@gmail.com">
-            <svg xmlns="http://www.w3.org/2000/svg" height="2.5em" viewBox="0 0 512 512"><path fill="#fff" d="M256 64C150 64 64 150 64 256s86 192 192 192c17.7 0 32 14.3 32 32s-14.3 32-32 32C114.6 512 0 397.4 0 256S114.6 0 256 0S512 114.6 512 256v32c0 53-43 96-96 96c-29.3 0-55.6-13.2-73.2-33.9C320 371.1 289.5 384 256 384c-70.7 0-128-57.3-128-128s57.3-128 128-128c27.9 0 53.7 8.9 74.7 24.1c5.7-5 13.1-8.1 21.3-8.1c17.7 0 32 14.3 32 32v80 32c0 17.7 14.3 32 32 32s32-14.3 32-32V256c0-106-86-192-192-192zm64 192a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z"/></svg>
+            <a
+              style={{
+                color: "white",
+                marginLeft: "1rem",
+                marginRight: "1rem",
+              }}
+              href="mailto:tomikahn2000@gmail.com"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="2.5em"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  fill="#fff"
+                  d="M256 64C150 64 64 150 64 256s86 192 192 192c17.7 0 32 14.3 32 32s-14.3 32-32 32C114.6 512 0 397.4 0 256S114.6 0 256 0S512 114.6 512 256v32c0 53-43 96-96 96c-29.3 0-55.6-13.2-73.2-33.9C320 371.1 289.5 384 256 384c-70.7 0-128-57.3-128-128s57.3-128 128-128c27.9 0 53.7 8.9 74.7 24.1c5.7-5 13.1-8.1 21.3-8.1c17.7 0 32 14.3 32 32v80 32c0 17.7 14.3 32 32 32s32-14.3 32-32V256c0-106-86-192-192-192zm64 192a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z"
+                />
+              </svg>
             </a>
           </div>
         </div>
